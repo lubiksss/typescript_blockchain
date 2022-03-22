2c25a8a7dd461769c4d9897cdd63a6fd955e49a5
jake <lubiksss@gmail.com>
2022-03-22 09:39:56 +0900

    init

---
+++ test.js
@@ -0,0 +1,9 @@
+const name = "Nicloas",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`)
+};
+
+sayHi(name, age);
