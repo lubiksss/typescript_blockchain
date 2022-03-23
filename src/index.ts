55483b3a824a3228cf0a0d0e6da01914b6f3bc0d
jake <lubiksss@gmail.com>
2022-03-23 10:47:09 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human{
-    name: string;
-    age: number;
-    gender: string;
-}
-const person = {
-    name: "jake",
-    age: 22,
-    gender: "male",
+class Human{
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
+const lynn = new Human("Lynn", 18, "female");
 const sayHi = (person:Human):string => {
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
 };
 
-console.log(sayHi(person))
+console.log(sayHi(lynn))
 
 
 export {};
